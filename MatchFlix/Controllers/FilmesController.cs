4ec349605f0b4845bda3ce11acb89a2f445df3f0
Eduardo Lopes <eduardohenrick900@gmail.com>
2026-06-04 19:00:00 -0300

    inserido mais dois controllers, desta vez a do grupo e do filmes

---
+++ MatchFlix/Controllers/FilmesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MatchFlix.Models;
+
+namespace MatchFlix.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Define a rota para o controlador, neste caso, "api/filmes"
+    public class FilmesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FilmesController(AppDbContext context) // Construtor para injetar o contexto do banco de dados
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Filme>>> ListarTodos() // Define que este método responde a requisições GET para "api/filmes"
+        {
+            var filmes = await _context.Filmes.ToListAsync();
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")] // Define que este método responde a requisições GET para "api/filmes/{id}"
+        public async Task<ActionResult<Filme>> BuscarPorId(int id)
+        {
+            var filme = await _context.Filmes.FindAsync(id); // Busca o filme pelo ID
+
+            if (filme == null)
+                return NotFound("Filme não encontrado."); // Retorna 404 se o filme não for encontrado
+
+            return Ok(filme); // Retorna o filme encontrado
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Filme>> Cadastrar(Filme novoFilme)
+        {
+            _context.Filmes.Add(novoFilme); // Adiciona o novo filme ao contexto
+            await _context.SaveChangesAsync(); // Salva as mudanças no banco de dados
+
+            return Ok(novoFilme); // Retorna o filme cadastrado
+        }
+    }
+}
---
+++ MatchFlix/Controllers/GruposController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MatchFlix.Models;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MatchFlix.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")] // Define a rota para o controlador, neste caso, "api/grupos"
+    public class GruposController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GruposController(AppDbContext context) // Construtor para injetar o contexto do banco de dados
+        {
+            _context = context;
+        }
+
+        [HttpPost] // Define que este método responde a requisições POST para "api/grupos"
+        public async Task<ActionResult<GrupoSessao>> CriarGrupo(GrupoSessao novoGrupo)
+        {
+            _context.GruposSessoes.Add(novoGrupo);
+            await _context.SaveChangesAsync(); // Salva as alterações no banco de dados
+
+            var membroCriador = new MembroGrupo
+            {
+                Id_Grupo = novoGrupo.Id_Grupo,
+                Id_Usuario = novoGrupo.Id_Usuario_Criador
+            };
+
+            _context.MembrosGrupos.Add(membroCriador);
+            await _context.SaveChangesAsync(); // Salva as alterações no banco de dados
+
+            return Ok(novoGrupo); // Retorna o grupo criado com um status 200 OK
+        }
+
+        [HttpPost("adicionar-membro")] // Define que este método responde a requisições POST para "api/grupos/adicionar-membro"
+        public async Task<IActionResult> AdicionarMembro(MembroGrupo novoMembro)
+        {
+            var JaEhMembro = await _context.MembrosGrupos
+                .AnyAsync(m => m.Id_Grupo == novoMembro.Id_Grupo && m.Id_Usuario == novoMembro.Id_Usuario);
+
+            if(JaEhMembro)
+                return BadRequest("Usuário já faz parte do grupo."); // Retorna um status 400 Bad Request se o usuário já for membro do grupo
+
+            _context.MembrosGrupos.Add(novoMembro);
+            await _context.SaveChangesAsync(); // Salva as alterações no banco de dados
+
+            return Ok("Membro adicionando com sucesso.");
+        }
+    }
+}
